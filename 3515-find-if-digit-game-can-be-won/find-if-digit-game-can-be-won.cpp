c8d892c8a377e8c9d3733b93b10b8ceef58ea76c
Aman <amankumar22011994@gmail.com>
2026-07-20 09:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (11.81%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int singleDigit = 0;
+        int doubleDigit = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] < 10) {
+                singleDigit += nums[i];
+            } else {
+                doubleDigit += nums[i];
+            }
+        }
+        if (singleDigit != doubleDigit) {
+            return true;
+        }
+        return false;
+    }
+};
