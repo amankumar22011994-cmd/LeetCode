e01a3ab88cff21ce4ee36fc2d4cbd36d7e08adff
Aman <amankumar22011994@gmail.com>
2026-08-15 00:00:55 +0530

    Time: 17 ms (43.96%) | Memory: 76.4 MB (12.66%) - LeetSync

---
+++ 816-design-hashset/design-hashset.cpp
@@ -0,0 +1,26 @@
+class MyHashSet {
+public:
+    bool arr[1000001];
+    MyHashSet() {
+        for (int i = 0; i <= 1000000; i++) {
+            arr[i] = false;
+        }
+    }
+    void add(int key) {
+        arr[key] = true;
+    }
+    void remove(int key) {
+        arr[key] = false;
+    }
+    bool contains(int key) {
+        return arr[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
