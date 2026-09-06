be30f57d86bb84c75681aa7fabe17050fbd21c05
Aman <amankumar22011994@gmail.com>
2026-09-06 22:54:04 +0530

    Time: 262 ms (27.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+select employee_id,department_id
+from Employee
+where primary_flag='Y'
+
+union all
+
+select employee_id,max(department_id)as department_id
+from Employee
+group by employee_id
+having count(*)=1
