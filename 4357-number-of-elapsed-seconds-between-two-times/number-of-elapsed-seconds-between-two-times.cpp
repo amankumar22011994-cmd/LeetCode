92af5953b2614ec60eb94f850828ea712761aab4
Aman <amankumar22011994@gmail.com>
2026-09-13 23:36:36 +0530

    Time: 2 ms (22.15%) | Memory: 10.3 MB (16.01%) - LeetSync

---
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSeconds(string time){
+        int h=stoi(time.substr(0,2));
+        int m=stoi(time.substr(3,2));
+        int s= stoi(time.substr(6,2));
+        return h * 3600 + m* 60+s;
+    }
+    int secondsBetweenTimes(string startTime, string endTime) {
+        return getSeconds(endTime)-getSeconds(startTime);
+    }
+};
